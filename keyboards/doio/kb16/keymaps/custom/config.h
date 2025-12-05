149d9b25ef0009c5d5d6177d7b62567e237b0170
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-12-05 17:47:21 +0000

    use bitbang for kb16 rbg, not working all white

---
+++ keyboards/doio/kb16/keymaps/custom/config.h
@@ -0,0 +1,3 @@
+#pragma once
+// 0.48 gives all white - better than 1 green LED
+#define WS2812_BITBANG_NOP_FUDGE 0.48
--- keyboards/doio/kb16/keymaps/custom/rules.mk
+++ keyboards/doio/kb16/keymaps/custom/rules.mk
@@ -1,8 +1,3 @@
-# Encoder enabled
 ENCODER_MAP_ENABLE = yes
-
-# Enable VIA support
 VIA_ENABLE = yes
-
-# Raw HID for layer broadcasting (VIA already includes this)
 RAW_ENABLE = yes
